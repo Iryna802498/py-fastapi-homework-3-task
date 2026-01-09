4cfe12516ccd8248f1d7dddd63a490252f4a043a
Iryna Mishchenko <djrobentab@gmail.com>
2026-01-09 15:53:10 +0100

    Solution

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -73,7 +73,7 @@ async def complete_reset_password(
     return response
 
 
-@router.post("/login/", status_code=200)
+@router.post("/login/", status_code=201)
 async def login(
     user_request: UserLoginRequest,
     db: AsyncSession = Depends(get_db),
